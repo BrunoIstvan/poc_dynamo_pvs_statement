65aa8c0de4b806bb54c07f6833bc6a067a1da7f0
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2021-10-13 06:15:06 -0300

    comentando código de execução

--- lambda_function.py
+++ lambda_function.py
@@ -6,10 +6,10 @@ def lambda_handler(event, context):
     return Main().execute(file_type=event['file_type'])
 
 
-if __name__ == '__main__':
-
-    event = {
-        'file_type': 'EEVD'
-    }
-
-    lambda_handler(event, None)
+# if __name__ == '__main__':
+#
+#     event = {
+#         'file_type': 'EEVD'
+#     }
+#
+#     lambda_handler(event, None)
